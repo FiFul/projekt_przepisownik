f9a3c99a9aeb530ece153f7778e77316aa0e8b7d
Dominik Gabis <dominikgabis04@gmail.com>
2025-06-18 16:02:56 +0200

    zintegrowano osobne okna w model master-detail

--- view/main_window.py
+++ view/main_window.py
@@ -1,7 +1,11 @@
-from PyQt5.QtWidgets import QMainWindow, QPushButton, QVBoxLayout, QWidget
-from view.recipe_list_view import RecipeListView
+from PyQt5.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QStackedWidget
+
 from view.calendar_list_view import CalendarListView
+from view.recipe_detail_view import RecipeDetailView
+from view.recipe_form import RecipeForm
+from view.recipe_list_view import RecipeListView
 from view.stats_view import StatsView
+
 
 class MainWindow(QMainWindow):
     def __init__(self, recipe_controller, calendar_controller):
@@ -11,32 +15,69 @@ class MainWindow(QMainWindow):
         self.recipe_controller = recipe_controller
         self.calendar_controller = calendar_controller
 
-        layout = QVBoxLayout()
+        # Główne widgety
+        main_widget = QWidget()
+        main_layout = QHBoxLayout(main_widget)
 
-        self.open_recipe_list_button = QPushButton("Przepisy")
-        self.open_recipe_list_button.clicked.connect(self.open_recipe_list)
-        layout.addWidget(self.open_recipe_list_button)
+        # Lewy panel (menu)
+        self.menu = QVBoxLayout()
+        self.btn_recipes = QPushButton("Przepisy")
+        self.btn_calendar = QPushButton("Kalendarz")
+        self.btn_stats = QPushButton("Statystyki")
+        self.btn_exit = QPushButton("Zamknij")
 
-        calendar_button = QPushButton("Kalendarz gotowania")
-        calendar_button.clicked.connect(self.open_calendar_list)
-        layout.addWidget(calendar_button)
+        self.btn_recipes.clicked.connect(self.show_recipes)
+        self.btn_calendar.clicked.connect(self.show_calendar)
+        self.btn_stats.clicked.connect(self.show_stats)
+        self.btn_exit.clicked.connect(self.close)
 
-        self.stats_button = QPushButton("Statystyki")
-        self.stats_button.clicked.connect(self.open_stats)
-        layout.addWidget(self.stats_button)
+        for btn in [self.btn_recipes, self.btn_calendar, self.btn_stats, self.btn_exit]:
+            self.menu.addWidget(btn)
+        self.menu.addStretch()
 
-        container = QWidget()
-        container.setLayout(layout)
-        self.setCentralWidget(container)
+        # Prawy panel (dynamiczne widoki)
+        self.stack = QStackedWidget()
+        self.recipe_list_view = RecipeListView(recipe_controller, self.calendar_controller)
+        self.calendar_list_view = CalendarListView(recipe_controller, calendar_controller)
+        self.stats_view = StatsView(recipe_controller, calendar_controller)
 
-    def open_recipe_list(self):
-        self.recipe_list_view = RecipeListView(self.recipe_controller, self.calendar_controller)
-        self.recipe_list_view.show()
+        self.stack.addWidget(self.recipe_list_view)   # index 0
+        self.stack.addWidget(self.calendar_list_view)      # index 1
+        self.stack.addWidget(self.stats_view)         # index 2
 
-    def open_calendar_list(self):
-        self.calendar_list_view = CalendarListView(self.recipe_controller, self.calendar_controller)
-        self.calendar_list_view.show()
+        # Połączenie layoutów
+        menu_widget = QWidget()
+        menu_widget.setLayout(self.menu)
+        menu_widget.setFixedWidth(160)
 
-    def open_stats(self):
-        self.stats_view = StatsView(self.recipe_controller, self.calendar_controller)
-        self.stats_view.show()
+        main_layout.addWidget(menu_widget)
+        main_layout.addWidget(self.stack)
+        self.setCentralWidget(main_widget)
+
+        self.show_recipes()
+
+    def show_recipes(self):
+        self.recipe_list_view.display_recipes(self.recipe_controller.db.recipes)
+        self.stack.setCurrentWidget(self.recipe_list_view)
+
+    def show_calendar(self):
+        self.calendar_list_view.display_recipes()
+        self.stack.setCurrentWidget(self.calendar_list_view)
+
+    def show_stats(self):
+        self.stats_view.update()
+        self.stack.setCurrentWidget(self.stats_view)
+
+    def show_recipe_detail(self, recipe):
+        detail_view = RecipeDetailView(recipe, self.recipe_controller, self.calendar_controller, self.show_recipes, self.show_edit_recipe)
+        self.stack.addWidget(detail_view)
+        self.stack.setCurrentWidget(detail_view)
+
+    def show_edit_recipe(self, recipe=None):
+        form = RecipeForm(recipe, self.recipe_controller, self.show_recipes)
+        self.stack.addWidget(form)
+        self.stack.setCurrentWidget(form)
+
+    def show_add_recipe(self):
+        self.show_edit_recipe(None)
+
